fa98d1c017d68301bc53e0815169ee325c4227fd
SSMalik99 <saravjeetsingh.malik@gmail.com>
2021-02-21 12:48:13 +0530

    yooh

---
+++ nodejs/nodeFromEdureka/1stNode/app.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const app = express();
+const port = 9900;
+
+const mongo = require('mongodb');
+const MongoClient = mongo.MongoClient;
+const mongourl = "mongodb://localhost:27017";
+let db;
+
+
+
+
+//default route
+app.get("/",(req,res)=>{
+    res.send("i'm runnig at default page");
+})
+
+//resaurent
+app.get("/restaurent",(req,res)=>{
+    db.collection('restaurent').find().toArray((err,result)=>{
+        if (err) throw err;
+        res.send(result);
+    });
+})
+
+//city
+app.get("/city",(req,res)=>{
+    db.collection('city').find().toArray((err,result)=>{
+        if (err) throw err;
+        res.send(result);
+    });
+})
+
+//reataurent aray with some condition for this purpose we will use params and query params
+//by using params
+app.get('/restaurent/:id',(req,res)=>{
+    var id = req.params.id;
+    db.collection('restaurent').find({city:id}).toArray((err,result)=>{
+        if (err) throw err;
+        res.send(result);
+    });
+})
+
+//mealtype
+app.get("/mealtype",(req,res)=>{
+    db.collection('mealType').find().toArray((err,result)=>{
+        if (err) throw err;
+        res.send(result);
+    });
+})
+
+//cuisine route
+app.get("/cuisine",(req,res)=>{
+    db.collection("cuisine").find().toArray((err,result)=>{
+        if(err) throw error;
+        res.send(result);
+    });
+})
+
+
+
+//connect mongodb and node js
+MongoClient.connect(mongourl,(error,connection)=>{
+    if (error) console.log(error);
+    db =connection.db('bubbyDB');
+
+    app.listen(port,(err)=>{
+        if (err) throw err;
+        console.log(`i m at ${port}`)
+    })
+})
+
+// MongoClient.connect(mongourl,(err,connection) => {
+//     if(err) console.log(err);
+//     db = connection.db('edurekinternship');
+  
+//     app.listen(port,(err) => {
+//       if(err) throw err;
+//       console.log(`Server is running on port ${port}`)
+//     })
+//   })
--- nodejs/nodeFromEdureka/1stNode/nodeExp.js
+++ nodejs/nodeFromEdureka/1stNode/nodeExp.js
@@ -5,15 +5,81 @@ const express = require("express");
 const app = express();
 let port = 8888;
 
+let cuisine =  [{
+    "_id": 1,
+    "name": "North Indian",
+    "cuisine": 1
+  },
+  {
+    "_id": 2,
+    "name": "South Indian",
+    "cuisine": 2
+  },
+  {
+    "_id": 3,
+    "name": "Chinese",
+    "cuisine": 3
+  },
+  {
+    "_id": 4,
+    "name": "Fast Food",
+    "cuisine": 4
+  },
+  {
+    "_id": 5,
+    "name": "Street Food",
+    "cuisine": 5
+  }
+];
+
+let location = [
+    {
+        "_id":1,
+	    "city_name":"Delhi",
+        "city":1,
+        "country_name" :"India"
+    },
+    {
+        "_id": 2,
+        "city_name":"Mumbai",
+        "city": 2,
+        "country_name":"India"
+    },
+    {
+        "_id": 3,
+        "city_name":"Pune",
+        "city": 3,
+        "country_name":"India"
+    },
+    {
+        "_id": 4,
+        "city_name":"Bangalore",
+        "city": 4,
+        "country_name":"India"
+    },
+    {
+        "_id": 5,
+	    "city_name":"Chandigarh",
+        "city": 5,
+        "country_name": "India"
+    }
+]
+
+
 app.get("/",(req,res)=>{
     res.send("i'm default routing");
 });
 
-app.get("/first",(req,res)=>{
-    res.send("i'm first routing");
-})
+app.get("/cuisine",(req,res)=>{
+    res.send(cuisine);
+});
+
+app.get("/location",(req,res)=>{
+    res.send(location);
+});
 
 app.listen(port,(error)=>{
     if (error) throw error;
     console.log(`i'm working at port number ${port}`);
 });
+
